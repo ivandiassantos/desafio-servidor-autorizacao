c44b31ef57724acb217a0056012a8be8c0a85578
ivan.dias.santos@gmail.com <ivan.dias.santos@gmail.com>
2019-04-10 11:02:29 -0300

    Correções no servidor de autorização

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -6,6 +6,12 @@ WHERE NOT EXISTS (
     SELECT username FROM user WHERE username = 'admin'
 ) LIMIT 1;
 
+INSERT INTO user (username, email, password, activated)
+SELECT * FROM (SELECT 'cliente', 'cliente@cliente.com', '$2a$10$r0RFDmpneBVryx.ihHK9gu6FFJQi4nTxQUqzdSTvrPpaKZMxigqpy', true) AS tmp
+WHERE NOT EXISTS (
+    SELECT username FROM user WHERE username = 'cliente'
+) LIMIT 1;
+
 INSERT INTO authority (name)
 SELECT * FROM (SELECT 'ROLE_USER') AS tmp
 WHERE NOT EXISTS (
@@ -19,9 +25,9 @@ WHERE NOT EXISTS (
 ) LIMIT 1;
 
 INSERT INTO user_authority (username, authority)
-SELECT * FROM (SELECT 'admin', 'ROLE_USER') AS tmp
+SELECT * FROM (SELECT 'cliente', 'ROLE_USER') AS tmp
 WHERE NOT EXISTS (
-    SELECT username, authority FROM user_authority WHERE username = 'admin' and authority = 'ROLE_USER'
+    SELECT username, authority FROM user_authority WHERE username = 'cliente' and authority = 'ROLE_USER'
 ) LIMIT 1;
 
 INSERT INTO user_authority (username, authority)
